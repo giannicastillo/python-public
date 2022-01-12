e21edf88e7c0ed7c661015f2103189bed876d4a9
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-01-12 15:13:21 -0500

    python basic functions assignment completed

---
+++ functions_basic_ii.py
@@ -0,0 +1,56 @@
+# def countdown(num):
+#     array=[];
+#     while (num>0):
+#         array.append(num)
+#         print (num)
+#         num-=1
+#     return array
+# b= countdown(5)
+# print(b)
+
+# def printandreturn(num,num2):
+#     print (num)
+#     return (num2)
+
+# a=printandreturn(1,2)
+
+# def startendsum(a,b):
+#     x= a+b
+#     return x
+# total = startendsum(1,5)
+# print (total)
+
+# array=[5,2,3,2,1,4]
+
+#4 option  1
+
+# def greaterthan2nd(a): 
+#     array=[];
+#     for i in range (0,len(a)):
+#         if(a[i]>a[1]):
+#             array.append(i)
+#     print(len(array))
+#     return array 
+
+# greaterthan2nd([5,2,3,2,1,4])
+
+#4 option 2 not finished 
+# def greaterthan2nd(lst):
+#     newList=[];
+#     for val in lst:
+#     print (val)
+#     if val>lst[1]:
+#         newList.append(val)
+#     print (len(newList))
+#     return newList
+
+#5 
+# def sizetovalue(size,value):
+#     array=[];
+#     for x in range(0,size,1): #the step = incraments of 1 
+#         array.append(value)
+#     return array
+
+# print(sizetovalue(4,7))
+
+
