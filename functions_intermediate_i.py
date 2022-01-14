8dbedc637cc089343227aec42bb497ca44e4b009
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-01-13 21:31:29 -0500

    function_intermediate_i.py (more review required)

---
+++ functions_intermediate_i.py
@@ -0,0 +1,11 @@
+x = [ [5,2,3], [15,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Bryant'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Andres', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+
